5dc9f23a13af135cda843455ebb253ff1382143b
SHAMMAS KT <164931284+shammashassan@users.noreply.github.com>
2026-09-24 22:51:45 +0530

    adding google search console verification tag inside layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "",
+  },
   openGraph: {
     title: `${portfolioData.personal.name} | ${portfolioData.personal.role}`,
     description: portfolioData.personal.tagline,
